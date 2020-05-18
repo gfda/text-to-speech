9a329edd100228f86c63960c57def83201fc518a
Gustavo Dias A <gfda@ecomp.poli.br>
2020-05-18 16:07:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#text-to-speech
+A simple text-to-speech program using:
+ * Python3 GUI ( [tkinter](https://docs.python.org/3/library/tkinter.html));
+ * Google Text-to-Speech Python lib( [gTTS](https://gtts.readthedocs.io/en/latest/index.html));
---
+++ source/TextToSpeech.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python3
+#
+#@author Gustavo Dias A.
+
+import os
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from gtts import gTTS 
+
+
+"""Background, text color and language"""
+BG_COLOR = "#33334d"
+TXT_COLOR = "#d9d9d9"
+LANGUAGE = 'en'
+
+class Application:
+
+    def __init__(self, master=None):
+        
+        root.title("Text to Speech")
+        
+        self.main_window = LabelFrame(master, text="Text to Speech in Python", bg=BG_COLOR, fg=TXT_COLOR)
+        self.main_window.pack(fill="both", expand="yes")
+        
+        """FIRST CONTAINER"""
+        self.first_container = Frame(self.main_window, pady=10, bg=BG_COLOR)
+        self.first_container.pack()
+
+        self.lbl_title = Label(self.first_container, text="Text to Speec - Using gTTS ", font="impact 16", bg=BG_COLOR, fg=TXT_COLOR)
+        self.lbl_title.pack()
+
+        """SECOND CONTAINER"""
+        self.second_container = Frame(self.main_window, padx=20, bg=BG_COLOR)
+        self.second_container.pack()
+
+        self.lbl_input_text = Label(self.second_container, text="Type your text here", bg=BG_COLOR, fg=TXT_COLOR)
+        self.lbl_input_text.pack(side=LEFT)
+
+        self.entry_input_text = Entry(self.second_container)
+        self.entry_input_text.pack(side=RIGHT)
+
+        """THIRD CONTAINER"""
+        self.third_container = Frame(self.main_window, pady=20, bg=BG_COLOR)
+        self.third_container.pack()
+
+        self.btn_exit = ttk.Button(self.third_container, text="Exit", command=self.main_window.quit)
+        self.btn_exit.pack(side=RIGHT, anchor="s") 
+
+        self.btn_select = ttk.Button(self.third_container, text='Speech', command=self.speak_text)
+        self.btn_select.pack(side=LEFT, anchor="s") 
+
+    def speak_text(self):
+        """Gets the text from entry_input_text, converts it using gTTS (Google Text To Speech)
+         and reproduces using the operating system command"""
+        typed_text = self.entry_input_text.get()
+        output_audio = gTTS(text=typed_text, lang=LANGUAGE, slow=False)
+        output_audio.save("../sound/output.mp3")
+        os.system("afplay ../sound/output.mp3")
+
+
+root = Tk()
+Application(root)
+root.mainloop()
